79c0a77793e2e72920acfe358b1baebee0b2125e
github username <github email address>
2018-11-21 16:17:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function  getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+    let nested = document.querySelector("#nested")
+    return nested.querySelector(".target")
+}
+
+function deepestChild(){
+  let grand = document.querySelector("#grand-node")
+  return grand.lastElementChild.lastElementChild.lastElementChild.lastElementChild
+}
+
+function increaseRankBy(n){
+
+  const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+
+    for (let i = 0; i < lis.length; i++) {
+      lis[i].innerHTML = (parseInt(lis[i].innerHTML) + parseInt(n)).toString();
+    }
+}
